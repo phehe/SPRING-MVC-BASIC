9f7e04c04d16c8dcb63236c567dffdb8c6d3228b
phehe <haeunoo@nate.com>
2021-11-09 20:35:40 +0900

    서블릿

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");      // HTTP 헤더
+        response.setCharacterEncoding("utf-8");     // HTTP 헤더
+        response.getWriter().write("hello " + username); // HTTP 메시지 바디에 들어감
+    }
+}
